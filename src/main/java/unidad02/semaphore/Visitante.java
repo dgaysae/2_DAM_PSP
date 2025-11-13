97b4af9288ac9ac7c2edf44a92c3e02b91c64cbe
dgaysae <dgaysae524@g.educaand.es>
2025-11-13 10:21:52 +0100

    Enhance Visitante.java with explanatory comments
    Added comments to clarify the semaphore logic for visitor behavior.

--- src/main/java/unidad02/semaphore/Visitante.java
+++ src/main/java/unidad02/semaphore/Visitante.java
@@ -27,18 +27,36 @@ public class Visitante implements Runnable {
         System.out.println(hilo.getName() + " - Llegando a casa");
         
         try {
+            /*
+            En el porche de la casa pueden entrar hasta 4 personas.
+            */
             asientos.acquire();
             System.out.println(hilo.getName() + " - se sienta en el porche.");
             
+            /*
+            La primera de las 4 personas del porche que haya pedido tocar el timbre,
+            tocará el timbre y entrará en la casa. En el porche quedarán 3 personas,
+            PERO seguimos teniendo 4 personas en total en la casa: 1 dentro y 3 en el porche.
+            */
             timbre.acquire();
             System.out.println(hilo.getName() + " - Tocando el timbre");
             Thread.sleep(1000);
             System.out.println(hilo.getName() + " - Fin tocar timbre");
             timbre.release();
 
+            /*
+            La persona ha salido de la casa, pero aún no ha salido del porche, por lo que no
+            puede acceder una nueva persona.
+            */
+
             System.out.println(hilo.getName() + " - deja libre un asiento en el porche.");
             System.out.println(hilo.getName() + " - entra en casa.");
             asientos.release();
+
+            /*
+            Ahora si que la persona ha abandonado la casa y otra de las que estaban esperando
+            fuera puede acceder al porche.
+            */
             
         } catch (InterruptedException ex) {}
 
