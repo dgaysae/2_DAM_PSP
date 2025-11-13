2a2d853efb79c917a41e93aa433e2a13456afa67
dgaysae <dgaysae524@g.educaand.es>
2025-11-13 06:50:51 +0100

    Actualizar Main.java
    Se han renombrado los semáforos y añadido comentario explicativo del ejercicio.

--- src/main/java/unidad02/semaphore/Main.java
+++ src/main/java/unidad02/semaphore/Main.java
@@ -3,17 +3,23 @@ package unidad02.semaphore;
 import java.util.concurrent.Semaphore;
 
 /**
- * 
+ * Este es un ejemplo de clase sobre semáforos anidados.
+ * Consiste en controlar a los visitantes que quieren entrar
+ * en una casa.
+ * La casa tiene un porche con 4 asientos para que 4 visitantes
+ * esperen a entrar.
+ * Sólo puede entrar un visitante a la casa y no entrará otro
+ * hasta que el anterior salga.
  * @author diego
  */
 public class Main {
     
     public static void main(String[] args) {
-        Semaphore asientos = new Semaphore(4, true);
-        Semaphore s = new Semaphore(1, true);
+        Semaphore sentarseEnAsiento = new Semaphore(4, true);
+        Semaphore entrarACasa = new Semaphore(1, true);
         
         for(int i = 0; i < 10; i++) {
-            new Visitante(s, asientos, "Pepe" + i).start();
+            new Visitante(entrarACasa, sentarseEnAsiento, "Pepe" + i).start();
         }
     }
     
